476a88e0513788275d490df81f33030ed23caf02
Your Name <18311017120@163.com>
2020-04-21 10:42:54 +0800

    初始化增加启动appium服务

--- runner/runner.py
+++ runner/runner.py
@@ -10,11 +10,15 @@ from config.path_config import TESTCASE_PATH, REPORT_PATH
 from common.update_desired_caps import UpdateDesiredCaps
 from common.base_install_app import IsInstallApp
 from common.base_log import log
+from common.base_appiumServer import AppiumServer
 
 suite = unittest.defaultTestLoader.discover(TESTCASE_PATH, pattern='test_*.py')
 
 if __name__ == '__main__':
     log.info("-----初始化测试环境-----")
+    # 启动appium服务
+    AppiumServer().start_appium()
+
     # 更新desired_caps
     UpdateDesiredCaps().update_desired_caps()
     # 判断测试机是否安装app，否则安装
@@ -32,4 +36,8 @@ if __name__ == '__main__':
     # 执行测试套件
     runner.run(suite)
     fp.close()
+
+    # 停止appium服务
+    AppiumServer().quit_appium()
+
     log.info("-----测试完成-----")
