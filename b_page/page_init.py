3eab2876659c6656c5a6e893fa31ba87c9e1ddf7
Your Name <18311017120@163.com>
2020-04-22 16:22:18 +0800

    用例初始化，登录进入首页

--- b_page/page_init.py
+++ b_page/page_init.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Time    : 2020/04/15
+# @Time    : 2020/04/20
 # @Author  : lfr
 
 import time
+import sys
 from b_page.basepage import BasePage
 from b_page.start_page import StartPage
 from b_page.login import LoginPage
@@ -18,8 +19,10 @@ class PageInit(BasePage):
     '''
     执行用例（登录用例除外）时先滑动引导页进入登录页面，并登录进入首页
     '''
-    x_ele = "立即更新"
+    login_ele = "登录"
+    update_ele = "立即更新"
     iknow_ele = "我知道了"
+    home_ele = "首页"
 
     def init_to_home(self):
         '''滑动引导页进入登录页面，登录进入首页'''
@@ -27,33 +30,44 @@ class PageInit(BasePage):
             mobile = get_yaml_value(TEST_ACCOUNT_PATH, 'account_1')
             pwd = get_yaml_value(TEST_ACCOUNT_PATH, 'pwd_1_right')
             StartPage(self.driver).swipe_start()
-            LoginPage(self.driver).login_opera(mobile, pwd)
-            time.sleep(2)
-
-            # 先判断是否有非强制更新弹窗，再判断是否有提示
-            self.is_exist_update(self.driver)
+            lp = LoginPage(self.driver)
+            lp.login_opera(mobile, pwd)
             time.sleep(1)
-            self.is_exist_iknow(self.driver)
-            time.sleep(1)
-            log.info("---登录成功，进入首页---")
+            # 判断是否登录成功
+            if lp.is_exist_element(self.update_ele):
+                log.info("---登录成功，进入首页---")
+                self.is_exist_update()
+                if lp.is_exist_element(self.iknow_ele):
+                    self.is_exist_iknow()
+                else:
+                    pass
+            elif lp.is_exist_element(self.iknow_ele):
+                log.info("---登录成功，进入首页---")
+                self.is_exist_iknow()
+            elif lp.is_exist_element(self.home_ele):
+                log.info("---登录成功，进入首页---")
+                pass
+            else:
+                log.error("初始化登录失败，终止自动化程序！！！")
+                sys.exit(0)
         except Exception as e:
             log.error("进入首页失败：{}".format(e))
 
-    def is_exist_update(self, driver):
+    def is_exist_update(self):
         '''判断登录后是否存在非强制更新弹窗，存在则点击取消按钮'''
-        result = self.is_exist_element(self.x_ele)
-        print(result)
+        result = self.is_exist_element(self.update_ele)
+        # print(result)
         if result == True:
-            UpdatePopup(driver).cancel_opera()
+            UpdatePopup(self.driver).cancel_opera()
         else:
             pass
 
-    def is_exist_iknow(self, driver):
+    def is_exist_iknow(self):
         '''登录后判断是否存在【我知道了】按钮，存在则点击'''
         result = self.is_exist_element(self.iknow_ele)
-        print(result)
+        # print(result)
         if result == True:
-            Iknow(driver).click_iknow_btn()
+            Iknow(self.driver).click_iknow_btn()
         else:
             pass
 
