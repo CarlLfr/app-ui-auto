14ceaf15e50abe9455147ca425fcafc2a950af8a
Your Name <18311017120@163.com>
2020-04-22 16:00:07 +0800

    增加退出登录用例

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -11,6 +11,9 @@ from common.base_operate import get_yaml_value
 from config.path_config import TEST_ACCOUNT_PATH
 from b_page.login import LoginPage
 from b_page.start_page import StartPage
+from b_page.update_popup import UpdatePopup
+from b_page.iknow import Iknow
+from b_page.my_set import MySet
 
 class TestLogin(unittest.TestCase):
     '''
@@ -27,9 +30,9 @@ class TestLogin(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_login_abnormal(self):
+    def test_01_login_abnormal(self):
         '''正确用户名，错误密码登录'''
-        log.info("---验证 正确用户名、正确密码登录---")
+        log.info("---验证正确用户名、正确密码登录---")
         tel = get_yaml_value(TEST_ACCOUNT_PATH, 'account_1')
         pwd = get_yaml_value(TEST_ACCOUNT_PATH, 'pwd_1_wrong')
         lp = LoginPage(self.driver)
@@ -39,17 +42,45 @@ class TestLogin(unittest.TestCase):
         self.assertTrue(result)
         time.sleep(2)
 
-    def test_login_normal(self):
+    def test_02_login_normal(self):
         '''正确用户名，正确密码登录'''
-        log.info("---验证 正确用户名、错误密码登录---")
+        log.info("---验证正确用户名、正确密码登录---")
         tel = get_yaml_value(TEST_ACCOUNT_PATH, 'account_1')
         pwd = get_yaml_value(TEST_ACCOUNT_PATH, 'pwd_1_right')
         lp = LoginPage(self.driver)
         lp.login_opera(tel, pwd)
         time.sleep(2)
-        result = lp.is_exist_element("立即升级")
+        # 判断是否登录成功
+        result = False
+        if lp.is_exist_element("立即更新"):
+            result = True
+            UpdatePopup(self.driver).cancel_opera()
+            if lp.is_exist_element("我知道了"):
+                Iknow(self.driver).click_iknow_btn()
+        else:
+            if lp.is_exist_element("我知道了"):
+                result = True
+                Iknow(self.driver).click_iknow_btn()
+            else:
+                if lp.is_exist_element("首页"):
+                    result = True
         self.assertTrue(result)
-        time.sleep(2)
+        time.sleep(1)
+
+    def test_03_logout(self):
+        '''退出登录'''
+        log.info("---验证退出登录---")
+        # 进入我的->设置
+        ms = MySet(self.driver)
+        ms.enter_to_my()
+        ms.enter_to_set()
+        # 点击退出登录
+        ms.logout_opera()
+
+        # 断言是否退出成功
+        time.sleep(1)
+        result = ms.is_exist_element("验证码登录")
+        self.assertTrue(result)
 
     def tearDown(self) -> None:
         pass
