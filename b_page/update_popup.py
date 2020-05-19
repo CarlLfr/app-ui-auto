fd12629ce9cb73b6eed5e1450ec2cf0eb9a9909b
lfr <lfr@qiekj.com>
2020-05-19 15:49:18 +0800

    更新、通知弹窗

--- b_page/update_popup.py
+++ b_page/update_popup.py
@@ -3,6 +3,7 @@
 # @Time    : 2020/04/16
 # @Author  : lfr
 
+import time
 from appium import webdriver
 from b_page.basepage import BasePage
 from b_page.login import LoginPage
@@ -12,11 +13,14 @@ from appium.webdriver.common.mobileby import MobileBy as By
 from common.base_driver import BaseDriver
 
 class UpdatePopup(BasePage):
-    '''版本更新弹窗'''
+    '''版本更新弹窗、通知弹窗'''
     update_loc = (By.ID, 'com.qekj.merchant:id/ll_update')
 
     cancel_el = 'com.qekj.merchant:id/iv_cancel'
     cancel_loc = (By.ID, cancel_el)
+
+    def notice_cancel_opera(self):
+        '''判断是否有通知弹窗'''
 
     def update_opera(self):
         '''更新版本'''
@@ -37,7 +41,11 @@ class UpdatePopup(BasePage):
         return upd_ele
 
 if __name__ == '__main__':
+    cancel_el = 'com.qekj.merchant:id/iv_cancel'
     driver = BaseDriver().android_driver()
     StartPage(driver).swipe_start()
     LoginPage(driver).login_opera('18768124236', '123456')
     UpdatePopup(driver).cancel_opera()
+    time.sleep(3)
+    boo = UpdatePopup(driver).is_exist_element(cancel_el)
+    print(boo)
